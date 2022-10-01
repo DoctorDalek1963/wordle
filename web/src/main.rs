00845e73b037fb1567e9463073077da9518f0f97
DoctorDalek1963 <dyson.dyson@icloud.com>
2022-10-01 16:26:14 +0100

    Implement dark mode persistence using `localStorage`

--- web/src/main.rs
+++ web/src/main.rs
@@ -5,6 +5,63 @@ use yew::prelude::*;
 
 mod board;
 
+fn storage_get_dark_mode() -> Option<bool> {
+    let storage = web_sys::window()?.local_storage().unwrap_or(None)?;
+    match storage.get_item("darkMode") {
+        Err(_) => None,
+        Ok(opt_str) => match opt_str {
+            None => None,
+            Some(value) => {
+                if value == "true" {
+                    Some(true)
+                } else if value == "false" {
+                    Some(false)
+                } else {
+                    None
+                }
+            }
+        },
+    }
+}
+
+fn storage_set_dark_mode(dark_mode: bool) -> Option<()> {
+    let storage = web_sys::window()?.local_storage().unwrap_or(None)?;
+    match storage.set_item("darkMode", &dark_mode.to_string()) {
+        Err(_) => None,
+        Ok(_) => Some(()),
+    }
+}
+
+fn set_dark_mode(dark_mode: bool) -> Option<()> {
+    let class_list = web_sys::window()?.document()?.body()?.class_list();
+
+    let a: &str;
+    let b: &str;
+
+    match dark_mode {
+        true => {
+            a = "light";
+            b = "dark";
+        }
+        false => {
+            a = "dark";
+            b = "light";
+        }
+    };
+
+    if class_list.contains(a) {
+        if let Err(_) = class_list.remove_1(a) {
+            return None;
+        };
+    };
+
+    if let Err(_) = class_list.add_1(b) {
+        return None;
+    };
+
+    Some(())
+}
+
 struct Model {
     game: Game,
     guesses: Vec<[Letter; 5]>,
@@ -35,6 +92,9 @@ impl Component for Model {
     }
 
     fn view(&self, ctx: &Context<Self>) -> Html {
+        let dark_mode = storage_get_dark_mode().unwrap_or(false);
+        set_dark_mode(dark_mode);
+
         let link = ctx.link();
         html! {
             <div class="game">
